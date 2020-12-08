74a19597ccebe72870109537b525df52827895e7
Valentin <pornin@hotmail.fr>
2020-12-08 22:14:51 +0100

    fetch update

--- src/creds.js
+++ src/creds.js
@@ -1,26 +1,12 @@
-function getToken() {
-
-    let url = 'https://eu.battle.net/oauth/token?client_id=3aa739b8c70c4af2993a6331178bd348&client_secret=HSBnqaMlHXHa3vQC6EJZOd8D5116F41k&grant_type=client_credentials'; //api endpoint as a string       
-    var temp  = await JsonFetch(url)
-    var token = await JsonFetch(url).access_token
-    return token ;
-}
-function test() {
-    document.write('<p>' + "coucou" + '</p>');
+function JsonFetch(url) {
+    return  fetch(url)
+                    .then(response => response.json())
+                    .catch(err => alert(err))
 }
 
-async function JsonFetch(url) {
-    return fetch(url)
-        .then((response) => {
-            console.log(response)
-            return response.json()
-        })
-        .then((data) => {
-            // Work with JSON data here 
-            console.log(data)
-            return data
-        })
-        .catch((err) => {
-            // Do something for an error here
-        })
+async function getToken(callback) {
+
+    let url = 'https://eu.battle.net/oauth/token?client_id=3aa739b8c70c4af2993a6331178bd348&client_secret=HSBnqaMlHXHa3vQC6EJZOd8D5116F41k&grant_type=client_credentials' //api endpoint as a string       
+    let token = (await JsonFetch(url)).access_token
+    callback(token)
 }
