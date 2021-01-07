3ca7203c0e3109f8075be8a8c59ffecffe63feac
Vincent Girardot <vincent.girardot@doctolib.com>
2021-01-07 23:42:44 +0100

    Filteeeeeeeers

--- js/slack-score/renderers/guild.js
+++ js/slack-score/renderers/guild.js
@@ -2,14 +2,48 @@ import { createPlayerElement, handleClickOnPlayer } from './player.js'
 import { formatDate } from '../core/date.js'
 import { CURRENT_PROGRESS_BOSS } from '../core/constants.js'
 
+let filters = {
+  role: 'any',
+  minMythicScore: 0,
+  maxMythicScore: 10000,
+  orderBy: 'none',
+}
+
 export const renderGuild = ({ rosters, progress }) => {
   document.getElementById('progress').className += 'hidden'
   document.getElementById('roster_1').classList.remove('vulcan-hidden')
   document.getElementById('roster_2').classList.remove('vulcan-hidden')
   document.getElementById('raid-progress').classList.remove('vulcan-hidden')
-  renderRoster(rosters[0], document.getElementById('roster_1_player_container'))
-  renderRoster(rosters[1], document.getElementById('roster_2_player_container'))
+  document.getElementById('filters').classList.remove('vulcan-hidden')
+  renderRoster(rosters[0], document.getElementById('roster_1_player_container'), filters)
+  renderRoster(rosters[1], document.getElementById('roster_2_player_container'), filters)
   renderRaidProgress(progress, document.getElementById('slider3'))
+
+  document.getElementById('role-filter').addEventListener('change', event => {
+    filters = { ...filters, role: event.target.value }
+    filterRoster.call({ rosters, filters })
+  })
+
+  document.getElementById('min-mythic-score-filter').addEventListener('blur', event => {
+    filters = { ...filters, minMythicScore: event.target.value || 0 }
+    filterRoster.call({ rosters, filters })
+  })
+
+  document.getElementById('max-mythic-score-filter').addEventListener('blur', event => {
+    filters = { ...filters, maxMythicScore: event.target.value || 10000 }
+    filterRoster.call({ rosters, filters })
+  })
+
+  document.getElementById('order-filter').addEventListener('change', event => {
+    filters = { ...filters, orderBy: event.target.value }
+    filterRoster.call({ rosters, filters })
+  })
+}
+
+function filterRoster() {
+  const { filters, rosters } = this
+  renderRoster(rosters[0], document.getElementById('roster_1_player_container'), filters)
+  renderRoster(rosters[1], document.getElementById('roster_2_player_container'), filters)
 }
 
 const renderRaidProgress = ({ normal, heroic, mythic }, parent) => {
@@ -30,13 +64,37 @@ const renderRaidProgressMode = (parent, dates) => {
   })
 }
 
-const renderRoster = (players, parent) => {
+const renderRoster = (players, parent, filters) => {
+  parent.innerHTML = '';
+
   let playerColElements = [
     createPlayerColElement(),
     createPlayerColElement(),
     createPlayerColElement()
   ]
 
+  players = players.filter(player => {
+    if (filters.role !== 'any' && filters.role !== player.role)
+      return false
+
+    if (player.progress.mythicPlusProgress.score < filters.minMythicScore)
+      return false
+
+    if (player.progress.mythicPlusProgress.score > filters.maxMythicScore)
+      return false
+
+    return true
+  })
+
+  if (filters.orderBy !== 'none') {
+    players = players.sort((playerA, playerB) => {
+      if (filters.orderBy === 'slackerFirst')
+        return playerA.progress.mythicPlusProgress.score - playerB.progress.mythicPlusProgress.score
+      else
+        return playerB.progress.mythicPlusProgress.score - playerA.progress.mythicPlusProgress.score
+    })
+  }
+  
   players.forEach((player, index) => {
     playerColElements[index%3].appendChild(createPlayerElement(player))
   })
